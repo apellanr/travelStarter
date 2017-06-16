ec2e12db8d98bf5b113a8f0f18d5e88138d2a284
Anna Thomas <anna.c.thomas1015@gmail.com>
2017-06-16 11:27:18 -0700

    travel home page

--- src/components/app.js
+++ src/components/app.js
@@ -14,6 +14,7 @@ const App = () => (
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/buildsearch">Build/Search Page</Link></li>
+
             </ul>
 
             <Route exact path="/" component={Home} />
---
+++ src/components/my_itineraries.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+
+export default()=> {
+    return(
+        <div>
+            <div>these are my saved itineraries</div>
+            <div>these are my drafts</div>
+        </div>
+    )
+}
