c7522706dd824486985c6e147a08837be1ad33ae
apellanr <ryan.apellanes@gmail.com>
2017-06-17 12:32:53 -0700

    initial setup for discover itinerary page

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,8 @@ import {
 import './app.css';
 import Home from './home';
 import BuildSearch from './buildsearch';
+import FindItin from './discover';
+
 
 const App = () => (
     <Router>
@@ -14,10 +16,12 @@ const App = () => (
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/buildsearch">Build/Search Page</Link></li>
+                <li><Link to="/discover">Find Itinerary</Link></li>
             </ul>
 
             <Route exact path="/" component={Home} />
             <Route path="/buildsearch" component={BuildSearch} />
+            <Route path="/discover" component={FindItin} />
         </div>
     </Router>
 );
