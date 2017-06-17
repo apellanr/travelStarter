bcbe2bf73d37d82a71aa4a25450202bfc46d21d1
apellanr <ryan.apellanes@gmail.com>
2017-06-17 13:43:59 -0700

    setup and link to page complete. will work on the building of component for page

--- src/components/app.js
+++ src/components/app.js
@@ -9,19 +9,39 @@ import Home from './home';
 import BuildSearch from './buildsearch';
 import FindItin from './discover';
 
+const nav_links = [
+    {
+        title: 'Home',
+        path: '/'
+    },
+    {
+        title: 'Destinations',
+        path:'/destinations'
+    },
+    {
+        title: 'Blog',
+        path: '/wanderlosters'
+    },
+    {
+        title: 'About',
+        path: '/about'
+    }
+];
 
 const App = () => (
     <Router>
         <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/buildsearch">Build/Search Page</Link></li>
-                <li><Link to="/discover">Find Itinerary</Link></li>
-            </ul>
+            <div className="container">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/buildsearch">Build/Search Page</Link></li>
+                    <li><Link to="/discover">Find Itinerary</Link></li>
+                </ul>
 
-            <Route exact path="/" component={Home} />
-            <Route path="/buildsearch" component={BuildSearch} />
-            <Route path="/discover" component={FindItin} />
+                <Route exact path="/" component={Home} />
+                <Route path="/buildsearch" component={BuildSearch} />
+                <Route path="/discover" component={FindItin} />
+            </div>
         </div>
     </Router>
 );
