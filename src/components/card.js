972442f6f7ec7c9b2a0d3c296d6849909aa5dd3d
alexsjlee <sjlee89@gmail.com>
2017-06-20 10:39:54 -0700

    Added basic card to search page

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const card = () => {
+const card = (props) => {
     return (
         <div className="container">
             <div className="card" style={{width: 20 + 'rem'}}>
-                <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%2840%29.jpg" alt="Card image cap"/>
+                <img className="card-img-top" src={props.img} alt="Card image cap"/>
                 <div className="card-block">
-                    <h4 className="card-title">Test Card</h4>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h4 className="card-title">{props.title}</h4>
+                    <p className="card-text">{props.text}</p>
                     <a href="#" className="btn btn-primary">Pls Work</a>
                 </div>
             </div>
--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Card from './card';
+import data from '../data';
 
 export default () => {
+    const list = data.recommendations.map((place, index) => {
+        return <Card key={index} title={place.name} text={place.snippet} img={place.images[0].source_url} />
+    });
+    console.log(data.recommendations);
     return(
         <div>
             <h1>Search Page</h1>
+                {list}
         </div>
     )
 }
