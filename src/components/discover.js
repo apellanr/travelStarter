4819f68b1725cd91085c1b9461d5f2c0bedaf22f
apellanr <ryan.apellanes@gmail.com>
2017-06-19 17:46:52 -0700

    setup for card component

--- src/components/discover.js
+++ src/components/discover.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from './card';
 import Footer from './footer';
 
 export default () => {
     return (
         <div>
             <h1>Find Itinerary Page</h1>
+            <Card />
             <Footer />
         </div>
     )
