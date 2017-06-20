3bb8f8af1e46dd5c785b7ace2199bb5f605788d9
Anna Thomas <anna.c.thomas1015@gmail.com>
2017-06-19 17:54:51 -0700

    data file added act

--- src/components/home.js
+++ src/components/home.js
@@ -5,21 +5,26 @@ import {
 } from 'react-router-dom';
 import  Build from './build';
 import Itinerary from './my_itineraries';
+import Discover from './discover';
+import Data from '../data';
 
 
 class Home extends Component{
   render() {
+      console.log ('Data is:', Data);
         return (
             <div>
                 <h1>Home Page</h1>
                 <ul>
-                    <li><Link to="#">Find Itinerary</Link></li>
+                    <li><Link to="/discover">Find Itinerary</Link></li>
                     <li><Link to="/my_itineraries">My Itineraries</Link></li>
                     <li><Link to="/buildsearch/build">Build</Link></li>
                 </ul>
 
                 <Route path ="./buildsearch/build" component={Build}/>
                 <Route path ="./my_itineraries" component={Itinerary}/>
+                <Route path ="./discover" component={Discover}/>
+
             </div>
         )
     }
