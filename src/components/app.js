616f0e4687c6642f0e1c60360080060e68d0c7a2
alexsjlee <sjlee89@gmail.com>
2017-06-23 11:11:49 -0700

    Installed redux-form and axios. Also put in a basic search input into the search page with validation

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
     Link,
     Route
 } from 'react-router-dom';
@@ -31,23 +30,16 @@ const nav_links = [
 ];
 
 const App = () => (
-    <Router>
+    <div>
+        <Navbar links={nav_links} brand="travelStarter"/>
         <div>
-            <Navbar links={nav_links} brand="travelStarter"/>
-            <div>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/buildsearch">Build/Search Page</Link></li>
-                    <li><Link to="/discover">Find Itinerary</Link></li>
-                </ul>
 
-                <Route exact path="/" component={Home} />
-                <Route path="/buildsearch" component={BuildSearch} />
-                <Route path="/my_itineraries" component={Itinerary}/>
-                <Route path="/discover" component={FindItin} />
-            </div>
+            <Route exact path="/" component={Home} />
+            <Route path="/buildsearch" component={BuildSearch} />
+            <Route path="/my_itineraries" component={Itinerary}/>
+            <Route path="/discover" component={FindItin} />
         </div>
-    </Router>
+    </div>
 );
 
 export default App;
--- src/components/search.js
+++ src/components/search.js
@@ -1,16 +1,75 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Card from './card';
+import { Field, reduxForm } from 'redux-form';
 import data from '../data';
 
-export default () => {
-    const list = data.recommendations.map((place, index) => {
-        return <Card key={index} title={place.name} text={place.snippet} img={place.images[0].source_url} />
-    });
-    console.log(data.recommendations);
-    return(
-        <div>
-            <h1>Search Page</h1>
-                {list}
-        </div>
-    )
+class SearchPage extends Component {
+    list() {
+        return data.recommendations.map((place, index) => {
+            return <Card key={index} title={place.name} text={place.snippet} img={place.images[0].source_url} />
+        });
+    }
+
+    renderField(field) {
+        const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`;
+        return(
+            <div className={className}>
+                <input
+                    className='form-control'
+                    type={field.type}
+                    { ...field.input }
+                />
+                <div className='form-control-feedback'>
+                    { field.meta.touched ? field.meta.error : '' }
+                </div>
+            </div>
+        )
+    }
+
+    onSubmit(val) {
+        console.log(val);
+    }
+
+    render() {
+        console.log(data.recommendations);
+        const { handleSubmit } = this.props;
+        return(
+            <div>
+                <h1>Search Page</h1>
+                <form onSubmit={handleSubmit((val) => { this.onSubmit(val) })}>
+                    <div>
+                        <Field 
+                            name='searchPage'
+                            type='text'
+                            component={this.renderField}
+                        />
+                        <span className='input-group-btn'>
+                            <button className='btn btn-secondary' type='submit'>Submit</button>
+                        </span>
+                    </div>
+                </form>
+                <div>
+                    {this.list()}
+                </div>
+            </div>
+        )
+    }
 }
+
+function validate(val) {
+    const errors  ={};
+
+    if(!val.searchPage) {
+        errors.searchPage = 'Please enter a search term.'
+    }
+
+    return errors;
+}
+
+SearchPage = reduxForm({
+    form: 'searchForm',
+    validate
+})(SearchPage);
+
+export default connect(null)(SearchPage);
