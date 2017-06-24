c9c0acf56126c9c4fb04ed41131cec00274a15c4
alexsjlee <sjlee89@gmail.com>
2017-06-24 16:40:41 -0700

    Added axios call

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import searchReducer from './search_reducer';
 
 const rootReducer = combineReducers({
-    form: formReducer
+    form: formReducer,
+    cityplaces: searchReducer
 })
 
 export default rootReducer;
