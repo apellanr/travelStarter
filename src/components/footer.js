8652f3032ca2bd15e7050671ea5e88b3cc26bf9a
apellanr <ryan.apellanes@gmail.com>
2017-06-23 19:57:03 -0700

    footer items are linking to the correct paths. need to figure out what the third option should be

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import {
+    Link,
+    Route
+} from 'react-router-dom';
+import Home from './home';
+import Search from './search';
+import FindItin from './discover';
 
 const footer = () => {
     return (
         <div id="footer">
-        <div className="navbar fixed-bottom navbar-inverse bg-default footer">
-            <div className="row" id="bottomNav">
-                <div className="col text-center icons"><a href="#" className="icon-text"><i className="material-icons">perm_identity</i></a></div>
-                <div className="col text-center icons"><a href="#" className="icon-text"><i className="material-icons">add</i></a></div>
-                <div className="col text-center icons"><a href="#" className="icon-text"><i className="material-icons">inbox</i></a></div>
+            <div className="navbar fixed-bottom navbar-inverse bg-default footer">
+                <div className="row" id="bottomNav">
+                    <div className="col text-center icons"><Link to="/" className="icon-text"><i className="material-icons">perm_identity</i></Link></div>
+                    <div className="col text-center icons"><Link to="/buildsearch/search" className="icon-text"><i className="material-icons">add</i></Link></div>
+                    <div className="col text-center icons"><a href="#" className="icon-text"><i className="material-icons">inbox</i></a></div>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
