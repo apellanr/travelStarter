2d23c6162086d3879f7ca5ba117821da3a1dc081
alexsjlee <sjlee89@gmail.com>
2017-06-26 16:34:42 -0700

    Changed the search results

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,11 @@ import axios from 'axios';
 import actions from './types';
 
 const BASE_URL = 'https://www.triposo.com/api/v2/poi.json?location_id=';
-const END_URL = '&count=25&fields=all&tag_labels=';
+const END_URL = '&count=100&fields=all&tag_labels=';
 const ACCOUNT = '2FYB6LGM';
 const TOKEN = 'lkuszx1cd7srxliatwfs0dalj0blvyis';
 
 export function fetchPlaces(city, query) {
-    window.location.query = `c=${city}&q=${query}`;
     const request = axios.get(`${BASE_URL}${city}${END_URL}${query}`, {
         params: {
             account: ACCOUNT,
