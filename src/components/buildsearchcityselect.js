66e3ef13f1ac7c170f720ed6a454c21a14612b83
apellanr <ryan.apellanes@gmail.com>
2017-06-26 12:39:02 -0700

    city selector setup

--- src/components/buildsearchcityselect.js
+++ src/components/buildsearchcityselect.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import {
     Link,
 } from 'react-router-dom';
+import CitySelect from './city_selector';
+import { citySelect } from './city_fxns';
 import Footer from './footer';
 
 class BuildSearchCitySelect extends Component {
     render() {
         return(
-            <div>
-                <h2 className="header-text">Select City</h2>
-                <ul>
-                    <li><Link to="/buildsearch/search/:id">Madrid</Link></li>
-                    <li><Link to="/buildsearch/search/:id">Orange County</Link></li>
-                </ul>
-
+            <div className="select-container">
+                <h2 className="header-text text-center">City Navigator</h2>
+                    <Link to="/buildsearch/search/:id"><CitySelect name="Los Angeles" className="la"/></Link>
+                    <Link to="/buildsearch/search/:id"><CitySelect name="Madrid" className="mad"/></Link>
                 <Footer />
             </div>
         )
