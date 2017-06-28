de8353a926c1b9c7009e4793ff47eaa2c9494e54
David Sung <dks99455@gmail.com>
2017-06-28 11:17:02 -0700

    php file structure base setup

--- prototypes/phpFileStructureProto/includes/createItems.php
+++ prototypes/phpFileStructureProto/includes/createItems.php
@@ -1,6 +1,95 @@
 <?php
-if(INTERNAL !== true){
+//check if INTERNAL is true.  If it isn't, or it isn't set, exit the code (die())
+if(INTERNAL !== true) {
     die('Error: cannot directly access.');
 }
 
+    //$entityBody = file_get_contents('php://input');
+    $entityBody = file_get_contents('includes/dummy.json');
+    $dataMaster= json_decode($entityBody,true);
+    $user_id = $dataMaster['userId'];
+    if(!empty($dataMaster['itinId'])){
+        $itin_id = $dataMaster['itinId'];
+    } else {
+        //generate a new itinerary id
+    }
+    //generate hash of data in json format
+    $data = $dataMaster['data'];
+
+    $itemStore = [];
+    $itemStore['id_hash'] = md5($entityBody);
+
+    //pull out necessary data out of data array
+////['image']['attribution']['attribution_link'];
+    $sections = $data['content']['sections'];
+
+
+    $itemStore['name'] = $data['name'];
+
+    $tags = $data['tags'];
+    $city_id = $tags[0]['tag']['location_id'];
+    $labels = [];
+    $snippets = [];
+    foreach($tags as $key=>$single_tag_data){
+        if(!empty($single_tag_data['tag']['label'])) {
+            $labels[] = $single_tag_data['tag']['label'];
+        }
+        if(!empty($single_tag_data['tag']['snippet'])){
+            $snippets[] = $single_tag_data['tag']['snippet'];
+        }
+    }
+    $itemStore['labels']=$labels;
+    $itemStore['snippets'] = $snippets;
+    $intro = $data['intro'];
+    $itemStore['intro']=$intro;
+    $itemStore['latitude'] = $data['coordinates']['latitude'];
+    $itemStore['longitude'] = $data['coordinates']['longitude'];
+    $facebook_id = $data['facebook_id'];
+    $itemStore['facebook_id'] = $facebook_id;
+    $images = [];
+    foreach($data['images'] as $imageSets){
+        foreach($imageSets['sizes'] as $imageData){
+            $images[] = $imageData['url'];
+        }
+    }
+    $itemStore['images']=$images;
+    $itemStore['place_id']= $data['id'];
+    $itemStore['details']= $entityBody;
+//    print("<pre>".print_r($itemStore,true)."</pre>");
+
+    //checks database for all existing activities and sends that data
+$name = $itemStore['name'];
+$place_id = $itemStore['place_id'];
+$longitude = $itemStore['longitude'];
+$latitude = $itemStore['latitude'];
+$images = ''.json_encode($itemStore['images']);
+$snippet = ''.json_encode($itemStore['snippets']);
+//$hashtags = [];
+//$tag_label = ;
+
+$query = "INSERT INTO `activities` SET `name` = $name, `place_id` = $place_id, `city_id` = $city_id, `longitude` = $longitude, `latitude` = $latitude, `images` = '$images', `snippet` = $snippet";
+
+//make a query to read all activities
+$result = mysqli_query($conn, $query);
+print($query);
+
+//check if $result is empty
+if(empty($result)) {
+    //if yes, report the error in the output
+    $output['errors'][] = 'result variable is empty';
+    //if no,     check if any results were passed in
+}else {
+    if(mysqli_affected_rows($conn) !== 0){
+        //if yes,
+        $output['success']= true;
+        //set the success to true
+        //do a while loop to fetch all the information, put it into an array
+        while($row = mysqli_fetch_assoc($result)){
+            $output['data'][] = $row;
+        }
+        //if no, report the error in the output
+    }else{
+        $output['errors'][]= 'No data.';
+    }
+}
 ?>
--- prototypes/phpFileStructureProto/includes/readItineraries.php
+++ prototypes/phpFileStructureProto/includes/readItineraries.php
@@ -3,8 +3,23 @@
 if(INTERNAL !== true){
     die('Error: cannot directly access.');
 }
+
 //checks database for all existing itineraries and sends that data
 $query = "SELECT `id`, `itinerary_name`, `creator_id`, `timestamp` FROM `itineraries`";
+
+/*  join request for itinerary, items_in_itinerary, and activities
+ * SELECT i.id AS itinerary_id, i.itinerary_name AS itinerary_name, i.creator_id,
+	IinI.index_in_itinerary,
+    a.name AS activity_name, a.city_id, a.latitude, a.longitude, a.tag_label AS category,
+    a.images AS image_list
+
+FROM itineraries AS i
+JOIN items_in_itinerary AS IinI
+	ON i.id = IinI.itinerary_id
+JOIN activities AS a
+	ON a.id = IinI.activity_id
+WHERE i.id = 1
+ */
 
 //make a query to read all itineraries
 $result = mysqli_query($conn, $query);
@@ -15,7 +30,7 @@ if(empty($result)) {
     $output['errors'][] = 'result variable is empty';
     //if no,     check if any results were passed in
 }else {
-    if(mysqli_affected_rows($conn) !== 0){
+    if(mysqli_num_rows($result) !== 0){
     //if yes,
     $output['success']= true;
     //set the success to true
