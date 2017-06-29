4b99f557c13bce1836fb268beeb82c13b42c1ffa
apellanr <ryan.apellanes@gmail.com>
2017-06-29 11:24:03 -0700

    actions and reducers for login created

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,5 +4,8 @@ export default {
     CURRENT_PAGE: 'current_page',
     CLOSE_ITINERARY: 'close_itinerary',
     ADD_PLACE: 'add_place',
-    ITINERARY_SELECTED: 'itinerary_selected'
+    ITINERARY_SELECTED: 'itinerary_selected',
+    SIGN_UP: 'sign-up',
+    SIGN_IN: 'sign-in',
+    ERROR: 'error'
 }
