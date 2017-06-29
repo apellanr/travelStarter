5d3c391c7d7dbc86778eb9a559ed2bc6080725f5
Anna Thomas <anna.c.thomas1015@gmail.com>
2017-06-29 13:12:52 -0700

    add comma in types

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,7 +5,7 @@ export default {
     CLOSE_ITINERARY: 'close_itinerary',
     ADD_PLACE: 'add_place',
     SELECT_ITINERARY: 'select_itinerary',
-    FETCH_ITINERARIES: 'fetch_itineraries'
+    FETCH_ITINERARIES: 'fetch_itineraries',
     ITINERARY_SELECTED: 'itinerary_selected',
     SIGN_UP: 'sign-up',
     SIGN_IN: 'sign-in',
