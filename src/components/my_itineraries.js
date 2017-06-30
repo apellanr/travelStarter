bd3e78a7f8970f1ea3a64aa0a76143a7fc92d9c0
Alex Lee <Alex Lee>
2017-06-30 13:16:59 -0700

    saved dummy data

--- src/components/my_itineraries.js
+++ src/components/my_itineraries.js
@@ -21,7 +21,7 @@ class ItineraryList extends Component {
                     <img className="card-img-top" src={itinerary.image} alt="Card image cap"/>
                     <div className="card-block">
                         <h4 className="card-title">{itinerary.place}</h4>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text">{itinerary.name}</p>
                         <a onClick = {()=> this.props.selectItinerary(itinerary)} className="btn btn-primary">select itinerary</a>
                     </div>
                 </div>
