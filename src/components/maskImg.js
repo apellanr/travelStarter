f9117c909d3beec1b0db36c0af07749f3e40ddef
apellanr <ryan.apellanes@gmail.com>
2017-07-20 18:52:57 -0700

    dev fixed

--- src/components/maskImg.js
+++ src/components/maskImg.js
@@ -7,16 +7,22 @@ import { createNewItin } from '../actions';
 //     'backgroundImage' : 'url(https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg)',
 // }
 
-export default props => {
-
-    const { header, location } = props;
-    return (
-        <div className={`jumbotron city-jumbo ${props.className}`}>
-            <div className="jumbo-content">
-                <h1 className={location === '/discover' ? 'display-4 text-center discover-header' : 'display-4 text-center' }>{header}</h1>
-                <p className="lead">
-                    <Link className={location === '/discover' ? 'hide' : 'btn btn-primary btn-lg'} to="/home" role="button">Get Started</Link>
-                </p>
+class MaskImg extends Component {
+    handleClick() {
+        console.log('Clicked!')
+        this.props.createNewItin();
+    }
+    
+    render() {
+        const { header, location } = this.props;
+        return (
+            <div className={`jumbotron city-jumbo ${this.props.className}`}>
+                <div className="jumbo-content">
+                    <h1 className={location === '/discover' ? 'display-4 text-center discover-header' : 'display-4 text-center' }>{header}</h1>
+                    <p className="lead">
+                        <Link className={location === '/discover' ? 'hide' : 'btn btn-primary btn-lg'} to="/home" role="button" onClick={() => this.handleClick()}>Get Started</Link>
+                    </p>
+                </div>
             </div>
         )
     }
