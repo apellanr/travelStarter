33ef15cc894144033e748588a42288c422e59f15
apellanr <ryan.apellanes@gmail.com>
2017-07-20 17:15:18 -0700

    sidenav hidden issue

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import MaskImg from './maskImg';
 
-const Splash = () =>  {
+const Splash = (props) =>  {
     return(
         <div>
+            <div className="nav-container">
+                <nav className="navbar fixed-top navbar-toggleable-md navbar-inverse bg-default">
+                    <a to="/home" className="navbar-brand splash-brand">travelStarter</a>
+                </nav>
+            </div>
             <MaskImg className="splash-img" header="Discover A New City" />
         </div>
     )
