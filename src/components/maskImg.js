7fa211b22f709cd8d7a833ba8d84c663e6c58ed5
apellanr <ryan.apellanes@gmail.com>
2017-07-24 10:09:27 -0700

    edits to splash page content

--- src/components/maskImg.js
+++ src/components/maskImg.js
@@ -18,8 +18,9 @@ class MaskImg extends Component {
         return (
             <div className={`jumbotron city-jumbo ${this.props.className}`}>
                 <div className="jumbo-content">
-                    <h1 className={location === '/discover' ? 'display-4 text-center discover-header' : 'display-4 text-center' }>{header}</h1>
-                    <p className="lead">
+                <h1 className={location === '/discover' ? 'display-4 text-center discover-header' : 'display-4 text-center' }>{header}</h1>
+                <p className={location === '/discover' ? 'hide' : 'lead text-center'} id={`${this.props.id}`}>travelStarter is a place where you can discover past trips or build your own trip from scratch.</p>
+                <p className="lead">
                         <Link className={location === '/discover' ? 'hide' : 'btn btn-primary btn-lg'} to="/home" role="button" onClick={() => this.handleClick()}>Get Started</Link>
                     </p>
                 </div>
